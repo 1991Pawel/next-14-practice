8bb3d2e6a9d29e5dd51f135c1db39976d3c9d6e5
Paweł Leśnik <lesnik.pawel1991@gmail.com>
2024-02-11 23:41:01 +0100

    add cms

---
+++ src/get-payload.ts
@@ -0,0 +1,40 @@
+import path from "path";
+import dotenv from "dotenv";
+import type { InitOptions } from "payload/config";
+import payload, { type Payload } from "payload";
+// import nodemailer from 'nodemailer'
+
+dotenv.config({
+	path: path.resolve(__dirname, "../.env"),
+});
+
+let cached = (global as any).payload;
+
+if (!cached) {
+	cached = (global as any).payload = {
+		client: null,
+		promise: null,
+	};
+}
+
+interface Args {
+	initOptions?: Partial<InitOptions>;
+}
+
+export const getPayloadClient = async ({ initOptions }: Args = {}): Promise<Payload> => {
+	if (!process.env.PAYLOAD_SECRET) {
+		throw new Error("PAYLOAD_SECRET is missing");
+	}
+	if (cached.client) {
+		return cached.client;
+	}
+
+	try {
+		cached.client = await cached.promise;
+	} catch (e: unknown) {
+		cached.promise = null;
+		throw e;
+	}
+
+	return cached.client;
+};
---
+++ src/payload.config.ts
@@ -0,0 +1,34 @@
+import { buildConfig } from "payload/config";
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { slateEditor } from "@payloadcms/richtext-slate";
+import { webpackBundler } from "@payloadcms/bundler-webpack";
+dotenv.config({
+	path: path.resolve(__dirname, "../.env"),
+});
+
+export default buildConfig({
+	serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
+	collections: [],
+	routes: {
+		admin: "sell",
+	},
+	admin: {
+		user: "users",
+		bundler: webpackBundler(),
+		meta: {
+			titleSuffix: "- DigitalHippo",
+			favicon: "/favicon.ico",
+			ogImage: "/thumbnail.jpg",
+		},
+	},
+	editor: slateEditor({}),
+	db: mongooseAdapter({
+		url: process.env.MONGODB_URL!,
+	}),
+	rateLimit: {
+		max: 2000,
+	},
+	typescript: {
+		outputFile: path.resolve(__dirname, "payload-types.ts"),
+	},
+});
