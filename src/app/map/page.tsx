3d83d54051af946cc824dd247f0f41027d3fc201
Paweł Leśnik <lesnik.pawel1991@gmail.com>
2024-02-17 18:25:36 +0100

    add map

---
+++ src/app/map/page.tsx
@@ -0,0 +1,12 @@
+import { MapComponent } from "@/components/MapComponent";
+
+
+export default function MapPage() {
+	return (
+		<div>
+    <h2>MAP PAGHEs</h2>
+    <MapComponent/>
+  
+</div>
+	);
+}
