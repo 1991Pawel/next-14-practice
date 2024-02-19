2654895ab2ab7928fbd56cd855d53e2e05ecd009
Paweł Leśnik <lesnik.pawel1991@gmail.com>
2024-02-19 18:41:41 +0100

    add

--- src/app/(add-offer)/sale/item/page.tsx
+++ src/app/(add-offer)/sale/item/page.tsx
@@ -1,5 +1,7 @@
 "use client";
-
+import { X, Link } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 export default function ItemPage() {
 	return (
 		<div>
@@ -7,6 +9,15 @@ export default function ItemPage() {
 			<h2>
 				Podziel się okazją z milionami ludzi Wklej link ze znalezioną okazją lub informacjami o niej
 			</h2>
+
+			<div className="mt-8 flex gap-5">
+				<div className="relative flex w-full  items-center ">
+					<Link className="absolute left-[12px] opacity-[0.5]" size={18} />
+					<Input className="pl-[42px]" placeholder="https://www.example.com/greatdeal..." />
+					<X color="red" className="absolute right-[12px] opacity-[0.5]" size={18} />
+				</div>
+				<Button>Zacznijmy</Button>
+			</div>
 		</div>
 	);
 }
