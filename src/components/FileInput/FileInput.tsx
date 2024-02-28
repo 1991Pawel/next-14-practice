3b40b595f0d5707ef62c64097224c7808457e436
Paweł Leśnik <lesnik.pawel1991@gmail.com>
2024-02-28 21:47:15 +0100

    handle isDragActive

--- src/components/FileInput/FileInput.tsx
+++ src/components/FileInput/FileInput.tsx
@@ -1,8 +1,8 @@
 "use client";
 import React, { type SyntheticEvent, useCallback, useEffect } from "react";
-import { useDropzone } from "react-dropzone";
+import { useDropzone, type Accept } from "react-dropzone";
 import { useFormContext } from "react-hook-form";
-import { X } from "lucide-react";
+import { X, FolderDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 const PhotosPlaceholder = () => {
@@ -19,14 +19,18 @@ const PhotosPlaceholder = () => {
 type PhotosProps = {
 	files: File[];
 	handleRemoveFile: (e: SyntheticEvent, indexFileToRemove: number) => void;
+	isDragActive: boolean;
 };
 
-const Photos = ({ files, handleRemoveFile }: PhotosProps) => {
+const Photos = ({ files, handleRemoveFile, isDragActive }: PhotosProps) => {
 	const placeholdersNumber = 5;
+
 	if (!files?.length) {
 		return (
 			<div className="relative mt-7 rounded-lg border-2 border-dashed   border-gray-300 p-4">
-				<div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform text-center">
+				<div
+					className={`absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform text-center ${isDragActive ? "opacity-0" : ""}`}
+				>
 					<Button className="" type="button">
 						Dodaj obrazki
 					</Button>
@@ -35,50 +39,67 @@ const Photos = ({ files, handleRemoveFile }: PhotosProps) => {
 						6000 px, a minimalny to 150 px)
 					</p>
 				</div>
-				<div className="grid grid-cols-2 gap-4  md:grid-cols-3 lg:grid-cols-4">
+				<div
+					className={`grid grid-cols-2 gap-4  md:grid-cols-3 lg:grid-cols-4 ${isDragActive ? "opacity-0" : ""}`}
+				>
 					{Array(placeholdersNumber)
 						.fill(null)
 						.map((_, i) => (
 							<div key={i} className="h-32 rounded-lg bg-gray-100 sm:h-40 md:h-48 lg:h-56"></div>
 						))}
 				</div>
-			</div>
-		);
-	} else {
-		return (
-			<div className="relative mt-7 rounded-lg border-2 border-dashed   border-gray-300 p-4">
-				<div className="grid grid-cols-2 gap-4  md:grid-cols-3 lg:grid-cols-4">
-					{files.map((file, index) => {
-						return (
-							<div
-								className="relative flex h-32 justify-center  rounded-lg bg-gray-100 sm:h-40 md:h-48 lg:h-56 "
-								key={file.name}
-							>
-								<div
-									className="z-index-10 absolute right-[12px] top-0 flex h-[32px] w-[32px] -translate-y-1/2 cursor-pointer items-center justify-center justify-items-center rounded-full bg-red-100"
-									onClick={(e) => handleRemoveFile(e, index)}
-								>
-									<X color="red" size={18} />
-								</div>
-								<img
-									className="block h-auto w-full object-contain"
-									src={URL.createObjectURL(file)}
-									alt={file.name}
-								/>
-							</div>
-						);
-					})}
-					<PhotosPlaceholder />
+				<div
+					className={`column absolute  left-1/2  top-1/2 flex w-[80%] -translate-x-1/2 -translate-y-1/2 transform flex-col text-center ${isDragActive ? "visible" : "hidden"}`}
+				>
+					<div className=" ml-auto mr-auto">
+						<FolderDown />
+					</div>
+					Upuść plik tutaj
 				</div>
 			</div>
 		);
 	}
+	return (
+		<div className="relative mt-7 rounded-lg border-2 border-dashed   border-gray-300 p-4">
+			<div className={`grid grid-cols-2 gap-4  md:grid-cols-3 lg:grid-cols-4 `}>
+				{files.map((file, index) => {
+					return (
+						<div
+							className={`relative flex h-32 justify-center  rounded-lg bg-gray-100 sm:h-40 md:h-48 lg:h-56 ${isDragActive ? "opacity-0" : ""}`}
+							key={file.name}
+						>
+							<div
+								className="z-index-10 absolute right-[12px] top-0 flex h-[32px] w-[32px] -translate-y-1/2 cursor-pointer items-center justify-center justify-items-center rounded-full bg-red-100"
+								onClick={(e) => handleRemoveFile(e, index)}
+							>
+								<X color="red" size={18} />
+							</div>
+							<img
+								className="block h-auto w-full object-contain"
+								src={URL.createObjectURL(file)}
+								alt={file.name}
+							/>
+						</div>
+					);
+				})}
+				<div
+					className={`column absolute  left-1/2  top-1/2 flex w-[80%] -translate-x-1/2 -translate-y-1/2 transform flex-col text-center ${isDragActive ? "visible" : "hidden"}`}
+				>
+					<div className=" ml-auto mr-auto">
+						<FolderDown />
+					</div>
+					Upuść plik tutaj
+				</div>
+				{!isDragActive && <PhotosPlaceholder />}
+			</div>
+		</div>
+	);
 };
 
 type FileInputProps = {
 	name: string;
 	mode: string;
-	accept?: string;
+	accept?: Accept | undefined;
 	type: string;
 };
 
@@ -124,9 +145,9 @@ export const FileInput = (props: FileInputProps) => {
 		setValue(name, updatedFiles, { shouldValidate: false });
 	};
 
-	const { getRootProps, getInputProps } = useDropzone({
+	const { getRootProps, getInputProps, isDragActive } = useDropzone({
 		onDrop,
-		accept: { accepted: [accept ?? ""] },
+		accept: accept,
 	});
 
 	useEffect(() => {
@@ -140,13 +161,13 @@ export const FileInput = (props: FileInputProps) => {
 		<div>
 			<div {...getRootProps()}>
 				<input
-					{...props}
+					// {...props}
 					className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
 					id={name}
 					{...getInputProps()}
 				/>
 
-				<Photos handleRemoveFile={handleRemoveFile} files={files} />
+				<Photos isDragActive={isDragActive} handleRemoveFile={handleRemoveFile} files={files} />
 			</div>
 		</div>
 	);
--- src/components/modules/add-offer/Form/ImagesForm.tsx
+++ src/components/modules/add-offer/Form/ImagesForm.tsx
@@ -3,22 +3,17 @@ import { useForm, FormProvider } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FileInput } from "@/components/FileInput/FileInput";
-const MAX_FILE_SIZE = 5000000;
-const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
+// const MAX_FILE_SIZE = 5000000;
+// const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
 const schema = z.object({
 	pictures: z.array(z.any()).refine(
 		(files: File[]) => {
+			console.log(files);
 			// Sprawdź, czy przynajmniej jeden plik został przesłany
-			if (!files || files.length === 0) return false;
-
-			const allImages = files.every((file) => ACCEPTED_IMAGE_TYPES.includes(file?.type));
-
-			const allUnderSizeLimit = files.every((file: File) => file?.size <= MAX_FILE_SIZE);
-
-			return allImages && allUnderSizeLimit;
+			return files && files.length > 0;
 		},
 		{
-			message: `At least one image is required. Only .jpg, .jpeg, .png, and .webp files are accepted. Max file size is 5MB.`,
+			message: `Zdjęcie jest wymagane`,
 		},
 	),
 });
@@ -34,16 +29,16 @@ export const ImagesForm = () => {
 		console.log("values", values);
 	};
 
+	const acceptFiles = {
+		"image/jpeg": [],
+		"image/png": [],
+	};
+
 	return (
 		<section>
 			<FormProvider {...methods}>
 				<form onSubmit={methods.handleSubmit(onSubmit)}>
-					<FileInput
-						accept="image/png, image/jpg, image/jpeg"
-						name="pictures"
-						mode="append"
-						type="file"
-					/>
+					<FileInput accept={acceptFiles} name="pictures" mode="append" type="file" />
 
 					<button> wyślij</button>
 					{methods?.formState?.errors?.pictures && (
